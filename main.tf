ec1c7ecb1daa2aea2d4e3c5e1020973169d2c72f
dandill-eng <61367130+dandill-eng@users.noreply.github.com>
2020-12-22 11:48:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x. 
   # If you are using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
   features {}
   subscription_id = "1234567yourdatahere89"
   client_id       = "1234567yourdatahere89" 
